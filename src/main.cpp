4850e1960bad8b0255552d4fa6fd5a1a5d08193f
Bepo <susanto@polibatam.ac.id>
2021-07-16 19:49:21 +0700

    Init

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+//OUTPUT
+#define R1 3
+#define R2 4
+#define R3 5
+#define R4 6
+#define R5 7
+#define R6 8
+#define R7 9
+#define R8 A3
+
+//INPUT
+#define S1 13
+#define S2 A0
+#define S3 A1
+#define S4 A2
+#define S5 12
+#define S6 11
+#define S7 10
+#define S8 2
+
+void setup() {
+
+}
+
+void loop() {
+
+}
